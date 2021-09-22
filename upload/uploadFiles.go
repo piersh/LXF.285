cf3249e5a12254d39b73c8ce38522a252ddfbf9b
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2021-09-22 19:13:21 +0300

    Upload files

--- upload/uploadFiles.go
+++ upload/uploadFiles.go
@@ -3,16 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
 
 var PORT = ":8008"
 
-func timeHandler(c *gin.Context) {
-	t := time.Now().Format(time.RFC1123)
-	c.String(http.StatusOK, t)
+func uploadHandler(c *gin.Context) {
+	name := c.PostForm("name")
+	email := c.PostForm("email")
+
+	// Source
+	file, err := c.FormFile("file")
+	if err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
+		return
+	}
+
+	filename := filepath.Base(file.Filename)
+	if err := c.SaveUploadedFile(file, filename); err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
+		return
+	}
+
+	c.String(http.StatusOK, fmt.Sprintf("File %s uploaded with fields name=%s and email=%s.", file.Filename, name, email))
 }
 
 func defaultHandler(c *gin.Context) {
@@ -23,10 +38,9 @@ func defaultHandler(c *gin.Context) {
 func main() {
 	fmt.Println("Starting HTTP server!")
 	router := gin.Default()
+	router.Static("/", "./public")
+	router.POST("/upload", uploadHandler)
 
-	router.GET("/time", timeHandler)
-	router.PUT("/time", timeHandler)
-	router.GET("/", timeHandler)
 	router.NoRoute(defaultHandler)
 	router.Run(PORT)
 }
