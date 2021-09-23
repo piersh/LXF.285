aa730b7cfc87aced05a5a365f241317389d39daa
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2021-09-23 22:49:12 +0300

    Logging data to file

---
+++ log/logging.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+var PORT = ":8008"
+var LOGFILE = "/tmp/gin.log"
+
+func timeHandler(c *gin.Context) {
+	t := time.Now().Format(time.RFC1123)
+	c.String(http.StatusOK, t)
+}
+
+func defaultHandler(c *gin.Context) {
+	c.String(http.StatusNotFound, "Connecting from "+c.ClientIP())
+}
+
+func main() {
+	gin.DisableConsoleColor()
+
+	router := gin.Default()
+
+	f, _ := os.Create(LOGFILE)
+	gin.DefaultWriter = io.MultiWriter(f)
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+
+	router.GET("/time", timeHandler)
+	router.PUT("/time", timeHandler)
+	router.GET("/", timeHandler)
+	router.NoRoute(defaultHandler)
+	router.Run(PORT)
+}
